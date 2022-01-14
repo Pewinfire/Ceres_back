ebb1152a3c00a12309ee0255c34d93c8cdeae7b5
Pewinfire <s.dung@hotmail.es>
2022-01-14 09:55:45 +0100

    shop terminado, market corregido y añadido getbyid

--- routes/market-routes.js
+++ routes/market-routes.js
@@ -3,29 +3,22 @@ const express = require("express");
 const { check } = require("express-validator");
 
 const marketControllers = require("../controllers/market-controllers");
-const market = require("../models/market");
 
 const router = express.Router();
 
 router.get("/", marketControllers.getMarkets);
 
+router.get("/:mid", marketControllers.getMarketById);
+
 router.post(
-    "/",
-    [
-      check("name").not().isEmpty(),
-      check("postalCode").isLength(5),
-      check("address").not().isEmpty(),
-    ],
+  "/",
+  [
+    check("name").not().isEmpty(),
+    check("postalCode").isLength(5),
+    check("address").not().isEmpty(),
+  ],
   marketControllers.createMarket
 );
 
-/* router.patch(
-  "/:mid",
-  [check("title").not().isEmpty(), check("description").isLength({ min: 5 })],  // solo la imagen 
-  placesControllers.updatePlaceById
-); // validacion
-
-
-router.delete("/:pid", placesControllers.deletePlaceById);*/
 
 module.exports = router;
